bf18ea081b306e492b3538bdd60ea717c0d679d6
lgarin211 <lagustinus211@gmail.com>
2025-07-22 02:34:33 +0700

    modified:   src/app/page.tsx 	modified:   src/app/sport-venue/page.tsx 	modified:   src/components/sections/FoodCourtSection.tsx

--- src/app/sport-venue/page.tsx
+++ src/app/sport-venue/page.tsx
@@ -2,14 +2,14 @@
 
 import { HeroSection } from "@/components/sections/sport-venue/HeroSection"
 import { VenueList } from "@/components/sections/sport-venue/VenueList"
-import { PromotionSection } from "@/components/sections/sport-venue/PromotionSection"
+// import { PromotionSection } from "@/components/sections/sport-venue/PromotionSection"
 
 export default function SportVenuePage() {
   return (
     <main className="min-h-screen">
       <HeroSection />
       <VenueList />
-      <PromotionSection />
+      {/* <PromotionSection /> */}
     </main>
   )
 }
